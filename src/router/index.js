9a6547d4fe68b320c099c123ad8b91ccef0425bd
Zhang Zheng <632893783@qq.com>
2023-08-25 15:33:15 +0800

    顺序调整

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,16 @@ export const routes = [
         redirect: '/mnist'
     },
     {
-        path: '/mnist',
-        // name: 'templateSyntax',
-        cName: '手写数字识别',
-        component: () => import('../views/mnist/index.vue')
-    },
-    {
         path: '/cifar10',
         // name: 'templateSyntax',
         cName: '图像10分类',
         component: () => import('../views/cifar10/index.vue')
+    },
+    {
+        path: '/mnist',
+        // name: 'templateSyntax',
+        cName: '手写数字识别',
+        component: () => import('../views/mnist/index.vue')
     }
 ]
 
