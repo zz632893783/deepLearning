ca971c38be1bec5fc128c56babbd1f357031f8d1
zz632893783 <15067777633@qq.com>
2023-08-25 09:33:01 +0800

    feat: 项目搭建

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import { router } from '@/router/index.js'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import '@/style/index.scss'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(router).use(ElementPlus).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+export const routes = [
+    {
+        path: '',
+        redirect: '/mnist'
+    },
+    {
+        path: '/mnist',
+        cName: '手写数字识别',
+        component: () => import('@/views/testPage.vue'),
+    },
+    {
+        path: '/cifar10',
+        cName: '图像10分类',
+        component: () => import('@/views/testPage.vue'),
+    }
+]
+
+export const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
