20ef2a1d65ec89acfce0037a4c71395832e560a5
Zhang Zheng <632893783@qq.com>
2023-08-25 12:26:36 +0800

    3

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './style/index.scss'
 import App from './App.vue'
-
+// import myComponent from '@/components/myComponent.vue'
+// import componentA from '@/components/componentA.vue'
+// import componentB from '@/components/componentB.vue'
+// import componentC from '@/components/componentC.vue'
 const app = createApp(App)
 app.use(router).use(ElementPlus).mount('#app')
+// app.component('my-component', {
+//     template: `<button>btn</button>`
+// })
+// app.component('my-component', myComponent)
+// app.component('component-a', componentA)
+//     .component('component-b', componentB)
+//     .component('component-c', componentC)
